79737d659a950dbc7aeb127c780800794ea34fbe
Alexandra Marie Miller <accouts@alexandra.systems>
2019-08-01 05:30:35 -0700

    finally semi-functional

--- install.sh
+++ install.sh
@@ -2,20 +2,22 @@
 
 REMINDER_BASHRC="
 # this starts the reminder program on shell login
-remind -j $HOME/.resources/scheduler/oneTime.rem
-remind -z $HOME/.resources/scheduler/master.rem
+(
+cd $HOME/.resources/scheduler/resources/
+remind -z master.rem &
+)
 "
 
 # make directory for resources
 if [ -d "resources" ]
 then
     mkdir -p ~/.resources/scheduler/
-    mv resources ~/.resources/scheduler/
+    cp -r resources ~/.resources/scheduler/
 fi
 
 # copy over resources
 mkdir -p $HOME/bin
-mv main.sh $HOME/bin/scheduler
+cp -r main.sh $HOME/bin/scheduler
 
 # add remind to .bashrc
 
--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@
 @creator: Alexandra Marie Miller
 @description
 A simple scheduler system designed to remind me of what to do every day.
-
+```
 USAGE: scheduler OPTIONS
   OPTIONS:
   -d --daily      TIME    MSG     display MSG at TIME each day
@@ -13,6 +13,7 @@ USAGE: scheduler OPTIONS
   -p --periodic   PERIOD  MSG     display MSG at the end of each PERIOD
   -l --list       GROUP           lists reminders in GROUP or all reminders
   -r --remove     ID              remove reminder by ID number
+```
 @description
 @dependencies: remind, bash
 '
@@ -27,6 +28,12 @@ OPTIONS:
 -l --list       GROUP           lists reminders in GROUP or all reminders
 -r --remove     ID              remove reminder by ID number
 "
+RESOURCES_DIR="$HOME/.resources/scheduler/resources"
+DAILY_REM_FILE="$RESOURCES_DIR/daily.rem"
+ONETIME_REM_FILE="$RESOURCES_DIR/oneTime.rem"
+PERIODIC_REM_FILE="$RESOURCES_DIR/periodic.rem"
+
+source $RESOURCES_DIR/var
 
 # ====================  FUNCTIONS  =============================================
 
@@ -36,15 +43,20 @@ OPTIONS:
   @description
 Converts argument to remind script and places it into the daily file.
   @description
-  dDependencies
+  @ependencies: none
   @args: time, msg
   @sideEffects: modifies daily file
-  @returnsi: none
+  @returns: none
   
 @function
 '
 daily () {
-    echo "test"
+    time=$1
+    shift
+    msg=$@
+    ID_NUM=`expr $ID_NUM + 1`
+    echo "# $ID_NUM" >> $DAILY_REM_FILE
+    echo -e "REM AT $time MSG $msg\n" >> $DAILY_REM_FILE
 }
 : '
 @function
@@ -52,6 +64,7 @@ daily () {
   @description
 Converts argument to remind script and places it in the one time file.
   @description
+  @ependencies: none
   @args: date, msg
   @sideEffects: modifies one time file
   @returns: none
@@ -59,7 +72,13 @@ Converts argument to remind script and places it in the one time file.
 @function
 '
 oneTime () {
-
+    date=$1
+    shift
+    msg=$@
+    ID_NUM=`expr $ID_NUM + 1`
+    echo "# $ID_NUM" >> $ONETIME_REM_FILE
+    echo "REM ONCE $date MSG $msg" >> $ONETIME_REM_FILE
+    echo "REM ONCE $date RUN sed \"/x$idNum/,+2d\" $ONETIME_REM_FILE" >> $ONETIME_REM_FILE
 }
 
 : '
@@ -68,6 +87,7 @@ oneTime () {
   @description
 Converts argument to remind script and places it into periodic file.
   @description
+  @ependencies: none
   @args: time period, msg
   @sideEffects: modifies periodic file
   @returns: none
@@ -75,7 +95,12 @@ Converts argument to remind script and places it into periodic file.
 @function
 '
 periodic () {
-
+    period=$1
+    shift
+    msg=$@
+    ID_NUM=`expr $ID_NUM + 1`
+    echo "# $ID_NUM" >> $PERIODIC_REM_FILE
+    echo -e "REM AT $period MSG $msg\n" >> $PERIODIC_REM_FILE
 }
 
 : '
@@ -91,6 +116,24 @@ Lists reminders by category or lists all reminders
 @function
 '
 list () {
+    if [ "$1" = "daily" ]
+    then
+        cat $DAILY_REM_FILE
+    elif [ "$1" = "onetime" ]
+    then
+        cat $ONETIME_REM_FILE
+    elif [ "$1" = "periodic" ]
+    then
+        cat $PERIODIC_REM_FILE
+
+    else
+        echo "DAILY REMINDERS:"
+        cat $DAILY_REM_FILE 
+        echo "ONETIME REMINDERS:"
+        cat $ONETIME_REM_FILE 
+        echo "PERIODIC REMINDERS:"
+        cat $PERIODIC_REM_FILE
+    fi
 
 }
 
@@ -108,21 +151,30 @@ list () {
 @function
 '
 remove () {
-
+    targetID=$1
+    sed "/x$targetID/,+1d" $ONETIME_REM_FILE $DAILY_REM_FILE $PERIODIC_REM_FILE
 }
+
+
+
 
 if [ $# -eq 0 ]
 then
     echo "$HELPMSG"
 
 else
-    [ "$1" = "-d" ] || [ "$1" = "--daily" ]
+    [ "$1" = "-d" ] || [ "$1" = "--daily" ] && shift && daily $@
 
-    [ "$1" = "-d" ] || [ "$1" = "--one-time" ]
+    [ "$1" = "-o" ] || [ "$1" = "--one-time" ] && shift && oneTime $@
     
-    [ "$1" = "-p" ] || [ "$1" = "--periodic" ]
+    [ "$1" = "-p" ] || [ "$1" = "--periodic" ] && shift && periodic $@
 
-    [ "$1" = "-r" ] || [ "$1" = "--remove" ]
-        
+    [ "$1" = "-l" ] || [ "$1" = "--list" ] && list $2
+
+    [ "$1" = "-r" ] || [ "$1" = "--remove" ] && remove $2
+
 fi
+
+# update var file
+echo "ID_NUM=\"$ID_NUM\"" > $RESOURCES_DIR/var
 
